29e184bc7e32dcae08fcbbaa94cac0fdd821faae
curso.ads <curso.ads@LAB06-50>
2022-08-15 21:59:01 -0300

    Adicionar arquivos de projeto.

---
+++ AULA 1 KATIA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA_1_KATIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // intanciação do objeto
+            Veiculo golf = new Veiculo();
+            Console.WriteLine("Digite a marca");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Digite o modelo");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Digite a placa");
+            string placa = Console.ReadLine();
+            Console.WriteLine("Digite o ano");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            // objeto chamando método atribuir
+            golf.atribuir(marca, modelo, placa, ano);
+            Console.WriteLine(golf.retorna());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AULA 1 KATIA/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA_1_KATIA
+{
+    public class Veiculo
+    {
+        // declaração de atributos
+        string marca;
+        string modelo;
+        string placa;
+        int ano;
+        // método para atribuir valores
+        public void atribuir(string ma, string mo, string pl, int a)
+        {
+            this.ano = a;
+            this.marca = ma;
+            this.modelo = mo;
+            this.placa = pl;
+        }
+        // método para retornar valores
+        public string retorna()
+        {
+            return "Marca = " + this.marca + "\nModelo = " + this.modelo + "\nPlaca = " + this.placa + 
+                "\nAno de fabricaçã o= " + this.ano;
+        }
+    }
+}
